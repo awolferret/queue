2324621c2cae75d326e573e0655a82172fcf2bda
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-21 19:43:16 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> money = new Queue<int>();
+            money.Enqueue(100);
+            money.Enqueue(150);
+            money.Enqueue(200);
+            int bank = 0;
+            int count = money.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                  bank += money.Dequeue();
+                  Console.WriteLine($"Денег на счету: {bank}");
+                  Console.ReadKey();
+                  Console.Clear();
+            }
+        }
+    }
+}
